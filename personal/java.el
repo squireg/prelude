fdaa1f66ccc93d7ce4ff4d8ea91c0cb582d1d53a
Geoff Squire <geoffrey.squire@gmail.com>
2014-02-10 15:07:35 +1100

    Import old java/eclim config

--- personal/java.el
+++ personal/java.el
@@ -1,3 +1,9 @@
 ;; Use eclim
 (prelude-require-packages '(emacs-eclim))
 
+(setq eclim-auto-save t
+      eclim-executable (expand-file-name "~/eclipse/eclim"))
+(global-eclim-mode)
+
+(setq compilation-scroll-output t)
+
